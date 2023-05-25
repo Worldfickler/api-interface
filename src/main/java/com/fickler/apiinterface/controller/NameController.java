7b38aab42b45c00281e337f0f571571357763949
fickler <2586844575@qq.com>
2023-05-25 23:24:57 +0800

    整合

--- src/main/java/com/fickler/apiinterface/controller/NameController.java
+++ src/main/java/com/fickler/apiinterface/controller/NameController.java
@@ -1,7 +1,10 @@
 package com.fickler.apiinterface.controller;
 
-import com.fickler.apiinterface.model.User;
+import com.fickler.apiclientsdk.model.User;
+import com.fickler.apiclientsdk.utils.SingUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 名称 API
@@ -24,7 +27,24 @@ public class NameController {
     }
 
     @PostMapping("/user")
-    public String getUsernameByPost(@RequestBody User user) {
+    public String getUsernameByPost(@RequestBody User user, HttpServletRequest request) {
+        String accessKey = request.getHeader("accessKey");
+        String nonce = request.getHeader("nonce");
+        String timestamp = request.getHeader("timestamp");
+        String sign = request.getHeader("sign");
+        String body = request.getHeader("body");
+        // todo 实际去数据库查看是否一分配给用户
+        if (!accessKey.equals("test")) {
+            throw new RuntimeException("无权限");
+        }
+        if (Long.parseLong(nonce) > 10000) {
+            throw new RuntimeException("无权限");
+        }
+        String serverSign = SingUtils.getSign(body, "abcdefg");
+        if (!sign.equals(serverSign)) {
+            throw new RuntimeException("无权限");
+        }
+        // todo 其他的校验
         return "POST 用户名字是" + user.getUsername();
     }
 
